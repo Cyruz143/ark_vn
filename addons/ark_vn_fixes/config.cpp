a078cbf2b4bc4c736e9da16973f8f83aa7dd8150
Cyruz143 <root.cyruz@gmail.com>
2022-07-14 22:49:18 +0100

    Cleanup

---
+++ addons/ark_vn_fixes/config.cpp
@@ -0,0 +1,10 @@
+class CfgPatches {
+    class ark_vn_fixes {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 2.08;
+        requiredAddons[] = {"ark_main"};
+        author = "ARK";
+        authorUrl = "http://www.ark-group.org";
+    };
+};
--- addons/ark_ww2_fixes/client_postinit.sqf
+++
@@ -1,2 +0,0 @@
-// Disalbe IF adding 343s to people when they get in vehicles
-LIB_GLOBAL_ENABLE_RADIO_IN_VEHICLE = false;
