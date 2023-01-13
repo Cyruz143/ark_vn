28fab786bdb17e4f69ede75cd73e68c2247f9f05
Cyruz143 <root.cyruz@gmail.com>
2023-01-13 10:19:50 +0000

    Add vests

---
+++ addons/ark_vn_vests/cfgWeapons.hpp
@@ -0,0 +1,372 @@
+class CfgWeapons {
+    class vn_b_vest_sog_base;
+    class vn_b_vest_base;
+    class vn_o_vest_base;
+    class vn_b_vest_diver_base;
+    class vn_b_vest_sog_01: vn_b_vest_sog_base {
+        HIDDEN
+    };
+    class vn_b_vest_sog_02: vn_b_vest_sog_base {
+        HIDDEN
+    };
+    class vn_b_vest_sog_03: vn_b_vest_sog_base {
+        HIDDEN
+    };
+    class vn_b_vest_sog_04: vn_b_vest_sog_base {
+        HIDDEN
+    };
+    class vn_b_vest_sog_05: vn_b_vest_sog_base {
+        HIDDEN
+    };
+    class vn_b_vest_sog_06: vn_b_vest_sog_base {
+        HIDDEN
+    };
+    class vn_b_vest_usarmy_01: vn_b_vest_base {
+        HIDDEN
+    };
+    class vn_b_vest_usarmy_11: vn_b_vest_base {
+        HIDDEN
+    };
+    class vn_b_vest_aircrew_02: vn_b_vest_base {
+        HIDDEN
+    };
+    class vn_o_vest_04: vn_o_vest_base {
+        HIDDEN
+    };
+    class vn_o_vest_01: vn_o_vest_base {
+        HIDDEN
+    };
+    class vn_b_vest_seal_01: vn_b_vest_diver_base {
+        HIDDEN
+    };
+    class vn_b_vest_seal_02: vn_b_vest_base {
+        HIDDEN
+    };
+    class vn_b_vest_anzac_01: vn_b_vest_base {
+        HIDDEN
+    };
+    class vn_b_vest_sas_01: vn_b_vest_base {
+        HIDDEN
+    };
+    class vn_b_vest_usarmy_02: vn_b_vest_usarmy_01 {
+        HIDDEN
+    };
+    class vn_b_vest_usarmy_03: vn_b_vest_usarmy_01 {
+        HIDDEN
+    };
+    class vn_b_vest_usarmy_04: vn_b_vest_usarmy_01 {
+        HIDDEN
+    };
+    class vn_b_vest_usarmy_05: vn_b_vest_usarmy_01 {
+        HIDDEN
+    };
+    class vn_b_vest_usarmy_06: vn_b_vest_usarmy_01 {
+        HIDDEN
+    };
+    class vn_b_vest_usarmy_07: vn_b_vest_usarmy_01 {
+        HIDDEN
+    };
+    class vn_b_vest_usarmy_08: vn_b_vest_usarmy_01 {
+        HIDDEN
+    };
+    class vn_b_vest_usarmy_09: vn_b_vest_usarmy_01 {
+        HIDDEN
+    };
+    class vn_b_vest_usarmy_10: vn_b_vest_usarmy_01 {
+        HIDDEN
+    };
+    class vn_b_vest_usarmy_12: vn_b_vest_usarmy_11 {
+        HIDDEN
+    };
+    class vn_b_vest_usarmy_13: vn_b_vest_usarmy_11 {
+        HIDDEN
+    };
+    class vn_b_vest_usarmy_14: vn_b_vest_usarmy_13 {
+        HIDDEN
+    };
+    class vn_b_vest_aircrew_01: vn_b_vest_usarmy_11 {
+        HIDDEN
+    };
+    class vn_b_vest_aircrew_03: vn_b_vest_aircrew_02 {
+        HIDDEN
+    };
+    class vn_b_vest_aircrew_04: vn_b_vest_aircrew_02 {
+        HIDDEN
+    };
+    class vn_b_vest_aircrew_05: vn_b_vest_aircrew_03 {
+        HIDDEN
+    };
+    class vn_b_vest_aircrew_06: vn_b_vest_aircrew_02 {
+        HIDDEN
+    };
+    class vn_b_vest_aircrew_07: vn_b_vest_aircrew_06 {
+        HIDDEN
+    };
+    class vn_o_vest_05: vn_o_vest_04 {
+        HIDDEN
+    };
+    class vn_o_vest_02: vn_o_vest_01 {
+        HIDDEN
+    };
+    class vn_o_vest_03: vn_o_vest_01 {
+        HIDDEN
+    };
+    class vn_o_vest_06: vn_o_vest_01 {
+        HIDDEN
+    };
+    class vn_o_vest_07: vn_o_vest_01 {
+        HIDDEN
+    };
+    class vn_o_vest_08: vn_o_vest_01 {
+        HIDDEN
+    };
+    class vn_o_vest_vc_01: vn_o_vest_01 {
+        HIDDEN
+    };
+    class vn_o_vest_vc_02: vn_o_vest_01 {
+        HIDDEN
+    };
+    class vn_o_vest_vc_03: vn_o_vest_01 {
+        HIDDEN
+    };
+    class vn_o_vest_vc_04: vn_o_vest_01 {
+        HIDDEN
+    };
+    class vn_o_vest_vc_05: vn_o_vest_01 {
+        HIDDEN
+    };
+    class vn_b_vest_seal_03: vn_b_vest_seal_02 {
+        HIDDEN
+    };
+    class vn_b_vest_seal_04: vn_b_vest_seal_02 {
+        HIDDEN
+    };
+    class vn_b_vest_seal_05: vn_b_vest_seal_02 {
+        HIDDEN
+    };
+    class vn_b_vest_seal_06: vn_b_vest_seal_02 {
+        HIDDEN
+    };
+    class vn_b_vest_seal_07: vn_b_vest_seal_02 {
+        HIDDEN
+    };
+    class vn_b_vest_anzac_02: vn_b_vest_anzac_01 {
+        HIDDEN
+    };
+    class vn_b_vest_anzac_03: vn_b_vest_anzac_01 {
+        HIDDEN
+    };
+    class vn_b_vest_anzac_04: vn_b_vest_anzac_01 {
+        HIDDEN
+    };
+    class vn_b_vest_anzac_05: vn_b_vest_anzac_01 {
+        HIDDEN
+    };
+    class vn_b_vest_anzac_06: vn_b_vest_anzac_01 {
+        HIDDEN
+    };
+    class vn_b_vest_anzac_07: vn_b_vest_anzac_01 {
+        HIDDEN
+    };
+    class vn_b_vest_anzac_08: vn_b_vest_anzac_01 {
+        HIDDEN
+    };
+    class vn_b_vest_anzac_09: vn_b_vest_anzac_01 {
+        HIDDEN
+    };
+    class vn_b_vest_anzac_10: vn_b_vest_anzac_01 {
+        HIDDEN
+    };
+    class vn_b_vest_sas_02: vn_b_vest_sas_01 {
+        HIDDEN
+    };
+    class vn_b_vest_sas_03: vn_b_vest_sas_01 {
+        HIDDEN
+    };
+    class vn_b_vest_sas_04: vn_b_vest_sas_01 {
+        HIDDEN
+    };
+    class vn_b_vest_sog_01_ARM: vn_b_vest_sog_01 {
+        ARMOUR
+    };
+    class vn_b_vest_sog_02_ARM: vn_b_vest_sog_02 {
+        ARMOUR
+    };
+    class vn_b_vest_sog_03_ARM: vn_b_vest_sog_03 {
+        ARMOUR
+    };
+    class vn_b_vest_sog_04_ARM: vn_b_vest_sog_04 {
+        ARMOUR
+    };
+    class vn_b_vest_sog_05_ARM: vn_b_vest_sog_05 {
+        ARMOUR
+    };
+    class vn_b_vest_sog_06_ARM: vn_b_vest_sog_06 {
+        ARMOUR
+    };
+    class vn_b_vest_usarmy_01_ARM: vn_b_vest_usarmy_01 {
+        ARMOUR
+    };
+    class vn_b_vest_usarmy_02_ARM: vn_b_vest_usarmy_02 {
+        ARMOUR
+    };
+    class vn_b_vest_usarmy_03_ARM: vn_b_vest_usarmy_03 {
+        ARMOUR
+    };
+    class vn_b_vest_usarmy_04_ARM: vn_b_vest_usarmy_04 {
+        ARMOUR
+    };
+    class vn_b_vest_usarmy_05_ARM: vn_b_vest_usarmy_05 {
+        ARMOUR
+    };
+    class vn_b_vest_usarmy_06_ARM: vn_b_vest_usarmy_06 {
+        ARMOUR
+    };
+    class vn_b_vest_usarmy_07_ARM: vn_b_vest_usarmy_07 {
+        ARMOUR
+    };
+    class vn_b_vest_usarmy_08_ARM: vn_b_vest_usarmy_08 {
+        ARMOUR
+    };
+    class vn_b_vest_usarmy_09_ARM: vn_b_vest_usarmy_09 {
+        ARMOUR
+    };
+    class vn_b_vest_usarmy_10_ARM: vn_b_vest_usarmy_10 {
+        ARMOUR
+    };
+    class vn_b_vest_usarmy_11_ARM: vn_b_vest_usarmy_11 {
+        ARMOUR
+    };
+    class vn_b_vest_usarmy_12_ARM: vn_b_vest_usarmy_12 {
+        ARMOUR
+    };
+    class vn_b_vest_usarmy_13_ARM: vn_b_vest_usarmy_13 {
+        ARMOUR
+    };
+    class vn_b_vest_usarmy_14_ARM: vn_b_vest_usarmy_14 {
+        ARMOUR
+    };
+    class vn_b_vest_aircrew_01_ARM: vn_b_vest_aircrew_01 {
+        ARMOUR
+    };
+    class vn_b_vest_aircrew_02_ARM: vn_b_vest_aircrew_02 {
+        ARMOUR
+    };
+    class vn_b_vest_aircrew_03_ARM: vn_b_vest_aircrew_03 {
+        ARMOUR
+    };
+    class vn_b_vest_aircrew_04_ARM: vn_b_vest_aircrew_04 {
+        ARMOUR
+    };
+    class vn_b_vest_aircrew_05_ARM: vn_b_vest_aircrew_05 {
+        ARMOUR
+    };
+    class vn_b_vest_aircrew_06_ARM: vn_b_vest_aircrew_06 {
+        ARMOUR
+    };
+    class vn_b_vest_aircrew_07_ARM: vn_b_vest_aircrew_07 {
+        ARMOUR
+    };
+    class vn_o_vest_04_ARM: vn_o_vest_04 {
+        ARMOUR
+    };
+    class vn_o_vest_05_ARM: vn_o_vest_05 {
+        ARMOUR
+    };
+    class vn_o_vest_01_ARM: vn_o_vest_01 {
+        ARMOUR
+    };
+    class vn_o_vest_02_ARM: vn_o_vest_02 {
+        ARMOUR
+    };
+    class vn_o_vest_03_ARM: vn_o_vest_03 {
+        ARMOUR
+    };
+    class vn_o_vest_06_ARM: vn_o_vest_06 {
+        ARMOUR
+    };
+    class vn_o_vest_07_ARM: vn_o_vest_07 {
+        ARMOUR
+    };
+    class vn_o_vest_08_ARM: vn_o_vest_08 {
+        ARMOUR
+    };
+    class vn_o_vest_vc_01_ARM: vn_o_vest_vc_01 {
+        ARMOUR
+    };
+    class vn_o_vest_vc_02_ARM: vn_o_vest_vc_02 {
+        ARMOUR
+    };
+    class vn_o_vest_vc_03_ARM: vn_o_vest_vc_03 {
+        ARMOUR
+    };
+    class vn_o_vest_vc_04_ARM: vn_o_vest_vc_04 {
+        ARMOUR
+    };
+    class vn_o_vest_vc_05_ARM: vn_o_vest_vc_05 {
+        ARMOUR
+    };
+    class vn_b_vest_seal_01_ARM: vn_b_vest_seal_01 {
+        ARMOUR
+    };
+    class vn_b_vest_seal_02_ARM: vn_b_vest_seal_02 {
+        ARMOUR
+    };
+    class vn_b_vest_seal_03_ARM: vn_b_vest_seal_03 {
+        ARMOUR
+    };
+    class vn_b_vest_seal_04_ARM: vn_b_vest_seal_04 {
+        ARMOUR
+    };
+    class vn_b_vest_seal_05_ARM: vn_b_vest_seal_05 {
+        ARMOUR
+    };
+    class vn_b_vest_seal_06_ARM: vn_b_vest_seal_06 {
+        ARMOUR
+    };
+    class vn_b_vest_seal_07_ARM: vn_b_vest_seal_07 {
+        ARMOUR
+    };
+    class vn_b_vest_anzac_01_ARM: vn_b_vest_anzac_01 {
+        ARMOUR
+    };
+    class vn_b_vest_anzac_02_ARM: vn_b_vest_anzac_02 {
+        ARMOUR
+    };
+    class vn_b_vest_anzac_03_ARM: vn_b_vest_anzac_03 {
+        ARMOUR
+    };
+    class vn_b_vest_anzac_04_ARM: vn_b_vest_anzac_04 {
+        ARMOUR
+    };
+    class vn_b_vest_anzac_05_ARM: vn_b_vest_anzac_05 {
+        ARMOUR
+    };
+    class vn_b_vest_anzac_06_ARM: vn_b_vest_anzac_06 {
+        ARMOUR
+    };
+    class vn_b_vest_anzac_07_ARM: vn_b_vest_anzac_07 {
+        ARMOUR
+    };
+    class vn_b_vest_anzac_08_ARM: vn_b_vest_anzac_08 {
+        ARMOUR
+    };
+    class vn_b_vest_anzac_09_ARM: vn_b_vest_anzac_09 {
+        ARMOUR
+    };
+    class vn_b_vest_anzac_10_ARM: vn_b_vest_anzac_10 {
+        ARMOUR
+    };
+    class vn_b_vest_sas_01_ARM: vn_b_vest_sas_01 {
+        ARMOUR
+    };
+    class vn_b_vest_sas_02_ARM: vn_b_vest_sas_02 {
+        ARMOUR
+    };
+    class vn_b_vest_sas_03_ARM: vn_b_vest_sas_03 {
+        ARMOUR
+    };
+    class vn_b_vest_sas_04_ARM: vn_b_vest_sas_04 {
+        ARMOUR
+    };
+};
---
+++ addons/ark_vn_vests/config.cpp
@@ -0,0 +1,17 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ark_vn_vests {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 2.00;
+        requiredAddons[] = {
+            "ark_vests",
+            "loadorder_f_vietnam"
+        };
+        author = "ARK";
+        authorUrl = "http://www.ark-group.org";
+    };
+};
+
+#include "cfgWeapons.hpp"
